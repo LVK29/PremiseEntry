0d7b6619c01520c244dbcaa8af185ab1e28376e1
Kartikeyan LV <48646649+LVK29@users.noreply.github.com>
2020-08-03 18:26:34 +0530

    Starter code

---
+++ digitalEntryPass/src/main/java/com/trinity/digitalEntryPass/DigitalEntryPassApplication.java
@@ -0,0 +1,14 @@
+package com.trinity.digitalEntryPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalEntryPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalEntryPassApplication.class, args);
+		System.out.println("yoyoyo");
+	}
+
+}
